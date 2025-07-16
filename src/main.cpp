77de4d841aab176109c8337822a8768a435d8cea
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2025-07-16 13:40:57 -0600

    refactor: comment out state manager and update loop for drive movements

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,9 @@
 #include "pose2d.hpp"
 #include "robot/statemanager.hpp"
 #include "subsystem/Drive/Drive.hpp"
+#include "robot/robot_instances.h"
 
-StateManager state_manager;
+//StateManager state_manager;
 
 const unsigned long UPDATE_INTERVAL = 50;
 
@@ -20,14 +21,25 @@ void setup()
 {
   Serial.begin(9600);
   Wire.begin();
-
-  state_manager.setState(RobotState::PICK_LOW_LEVEL);
+  
+  //state_manager.setState(RobotState::INIT);
+  
   interrupts();
 }
 
 void loop()
 {
-  state_manager.update();
+  //state_manager.update();
+  //drive_.update();
+
+  drive_.moveForward(100);
+  delay(10000);
+  drive_.moveBackward(100);
+  delay(10000);
+  drive_.moveLeft(100);
+  delay(10000);
+  drive_.moveRight(100);
+  delay(10000);
 
   delay(UPDATE_INTERVAL);
 }
