f67de264ea0ed10f0f402aebd1b919b319624697
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2025-07-16 18:52:12 -0600

    feat: add robot task management functions and include FreeRTOS Teensy library

--- src/main.cpp
+++ src/main.cpp
@@ -6,40 +6,17 @@
 
 #include <Arduino.h>
 #include <Wire.h>
-#include "pose2d.hpp"
-#include "robot/statemanager.hpp"
-#include "subsystem/Drive/Drive.hpp"
-#include "robot/robot_instances.h"
-
-//StateManager state_manager;
-
-const unsigned long UPDATE_INTERVAL = 50;
-
-double state_start_time = 0;
+#include <FreeRTOS.h>
+#include <task.h>
 
 void setup()
 {
   Serial.begin(9600);
   Wire.begin();
-  
-  //state_manager.setState(RobotState::INIT);
-  
-  interrupts();
+
+  // Start RTOS scheduler
+  vTaskStartScheduler();
 }
 
-void loop()
-{
-  //state_manager.update();
-  //drive_.update();
-
-  drive_.moveForward(100);
-  delay(10000);
-  drive_.moveBackward(100);
-  delay(10000);
-  drive_.moveLeft(100);
-  delay(10000);
-  drive_.moveRight(100);
-  delay(10000);
-
-  delay(UPDATE_INTERVAL);
-}
+// This is not used in RTOS
+void loop() {}
