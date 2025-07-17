7969c2d0af8c1cc41aef26321e23dd8fc11717c8
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2025-07-16 19:20:56 -0600

    feat: implement FreeRTOS task setup and update global update task for improved robot task management

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,15 @@
 #include <FreeRTOS.h>
 #include <task.h>
 
+#include "robot/tasks.h"
+
 void setup()
 {
   Serial.begin(9600);
   Wire.begin();
+
+  // FreeRTOS setup
+  setupTasks();
 
   // Start RTOS scheduler
   vTaskStartScheduler();
--- src/robot/tasks.h
+++ src/robot/tasks.h
@@ -3,26 +3,14 @@
 #include "PID_v1.h"
 #include "robot_instances.h"
 #include "../lib/math/pose2d.hpp"
+#include <FreeRTOS.h>
+#include <task.h>
+#include "../include/constants/constants.h"
 
-bool centerWithObject(double elapsed_time);
-bool alignWithObject(double elapsed_time, float desired_distance);
+using namespace Constants;
 
-bool initStart();
-bool exitStart(double elapsed_time);
+TaskHandle_t globalUpdateTaskHandle;
 
-bool goTreeZone(double elapsed_time);
-bool goStorageZone(double elapsed_time);
+void setupTasks();
 
-bool avoidPool(double elapsed_time);
-
-bool goLeftLimit(double elapsed_time);
-bool goRightLimit(double elapsed_time);
-
-bool searchForTrees(double elapsed_time, bool direction);
-bool goStorage(double elapsed_time, int container_type);
-
-bool dropBeans(double elapsed_time, int container_type);
-
-bool pickBean(double elapsed_time, int level);
-
-bool globalUpdate();
+void globalUpdateTask(void *pvParameters);
