fe844c1ef3d7e28abe1c58a809bc9ebd816e7ace
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2025-09-02 19:00:30 -0600

    refactor: initialize line and distance sensors in setup and update loop to read line sensor data

--- src/main.cpp
+++ src/main.cpp
@@ -13,20 +13,32 @@ void setup()
 {
   Serial.begin(9600);
   Serial.println("Starting...");
-  Wire.begin(); 
+  Wire.begin();
 
   // All systems must begin after initializing the serial and as the code starts running
   drive_.begin();
   elevator_.begin();
   com_.begin();
-
+  line_sensor_.begin();
+  distance_sensor_.begin();
+  
   drive_.setState(0),
   drive_.acceptHeadingInput(Rotation2D::fromDegrees(0));
 }
 
 void loop()
 {
-  drive_.update();
-  delay(50);
-  //distance_sensor_.getArrayDistance();
+  // drive_.update();
+  // delay(50);
+  auto line_sensor_data = line_sensor_.readSensors();
+  for (int i = 0; i < line_sensor_data.size(); i++)
+  {
+    Serial.print(line_sensor_data[i]);
+    if (i < line_sensor_data.size() - 1)
+    {
+      Serial.print(" ");
+    }
+  }
+  Serial.println();
+  // distance_sensor_.getArrayDistance();
 }
