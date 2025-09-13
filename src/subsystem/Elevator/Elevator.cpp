2bf0feec38cf68f1f03ca143de3b44dbb90d8ada
Héctor Tovar <soyeduardoo20@gmail.com>
2025-09-13 17:06:09 -0600

    refactor: update Elevator class constructor and methods for improved functionality and clarity

--- src/subsystem/Elevator/Elevator.cpp
+++ src/subsystem/Elevator/Elevator.cpp
@@ -1,52 +1,103 @@
 #include "Elevator.hpp"
-#include <Arduino.h>
 
-Elevator::Elevator()
-{
-    pinMode(Pins::kDirElevator, OUTPUT);
-    pinMode(Pins::kStepElevator, OUTPUT);
+//* Constructor
+Elevator::Elevator(int stepPin, int dirPin)
+    : left_step_pin_(stepPin), left_dir_pin_(dirPin),
+      right_step_pin_(stepPin), right_dir_pin_(dirPin) {}
+
+//* Setup method to initialize the motors
+bool Elevator::setup() {
+    //moveDown(kUpperLevel); // Ensure that the elevator is at the bottom at the start of the game
+    current_height_ = ElevatorConstants::kUpperLevel; //para pruebas del sábado
+    Serial.println("Elevator initialized.");
+    return true; // Return true when setup is successful
 }
 
-void Elevator::begin()
-{
-    digitalWrite(Pins::kDirElevator, HIGH);
-}
-
-void Elevator::update()
-{
-
-}
-
-void Elevator::setState(int state)
-{
-    switch (state)
-    {
-    case 0: // HOME
-        target_position_ = ElevatorConstants::kIdleLevel;
-        break;
-    case 1: // LOWER
-        target_position_ = ElevatorConstants::kLowerLevel;
-        break;
-    case 2: // MID
-        target_position_ = ElevatorConstants::kMidLevel;
-        break;
-    case 3: // UPPER
-        target_position_ = ElevatorConstants::kUpperLevel;
-        break;
+void Elevator::update() {
+    switch (elevator_state_) {
+        case ElevatorState::HOME:
+            moveToHeight(ElevatorConstants::kUpperLevel);
+            break;
+        case ElevatorState::LOW_POSITION:
+            moveToHeight(ElevatorConstants::kLowerLevel);
+            break;
+        case ElevatorState::MID_POSITION:
+            moveToHeight(ElevatorConstants::kMidLevel);
+            break;
+        case ElevatorState::HIGH_POSITION:
+            moveToHeight(ElevatorConstants::kUpperLevel);
+            break;
     }
 }
 
-bool Elevator::getLimitState()
-{
+void Elevator::setState(int state) {
+    elevator_state_ = static_cast<ElevatorState>(state);
 }
 
-void Elevator::resetPosition(double position)
-{
-    Serial.println(position);
-    current_position_ = position;
+//* Method to move the elevator up by a specified distance in cm
+void Elevator::moveUp(float distance) {
+    int steps = translateCmToSteps(distance); // Convert distance to steps
+
+    // Serial.print("Elevator moving up: ");
+    // Serial.print(distance);
+    // Serial.println(" cm");
+
+    digitalWrite(left_dir_pin_, HIGH);
+    digitalWrite(right_dir_pin_, LOW);
+    
+    for (int i = 0; i < steps; i++) {
+        digitalWrite(left_step_pin_, HIGH);
+        digitalWrite(right_step_pin_, HIGH);
+        delayMicroseconds(1000);
+
+        digitalWrite(left_step_pin_, LOW);
+        digitalWrite(right_step_pin_, LOW);
+        delayMicroseconds(1000);
+    }
 }
 
-int Elevator::getCurrentPosition()
-{
-    return current_position_;
+//* Method to move the elevator down by a specified distance in cm
+void Elevator::moveDown(float distance) {
+    int steps = translateCmToSteps(distance); // Convert distance to steps
+
+    // Serial.print("Elevator moving down: ");
+    // Serial.print(distance);
+    // Serial.println(" cm");
+
+    digitalWrite(left_dir_pin_, LOW);
+    digitalWrite(right_dir_pin_, HIGH);
+    
+    for (int i = 0; i < steps; i++) {
+        digitalWrite(left_step_pin_, HIGH);
+        digitalWrite(right_step_pin_, HIGH);
+        delayMicroseconds(1000);
+        digitalWrite(left_step_pin_, LOW);
+        digitalWrite(right_step_pin_, LOW);
+        delayMicroseconds(1000);
+    }
+}
+
+//* Method to move to a specific height in cm
+void Elevator::moveToHeight(float height) {
+    if (height > current_height_) {
+        moveUp(height - current_height_);
+    } else if (height < current_height_) {
+        moveDown(current_height_ - height);
+    }
+    current_height_ = height; // Update the current height
+}
+
+//* Stop the motors
+void Elevator::stop() {
+    // Serial.println("Elevator stops");
+
+    digitalWrite(left_step_pin_, LOW);
+    digitalWrite(right_step_pin_, LOW);
+}
+
+/* Private methods */
+
+//* Method to convert a distance in centimeters to the corresponding number of steps. Returns number of steps required to move the given distance
+int Elevator::translateCmToSteps(float distance) {
+    return (int)((distance / (2 * M_PI * ElevatorConstants::kStepperRadius)) * ElevatorConstants::kStepsPerRevolution);
 }
--- src/subsystem/Elevator/Elevator.hpp
+++ src/subsystem/Elevator/Elevator.hpp
@@ -1,37 +1,52 @@
-#pragma once
+#ifndef ELEVATOR_H
+#define ELEVATOR_H
 
 #include <Arduino.h>
-#include "constants/constants.h"
 #include "../systems/system.hpp"
-#include "constants/pins.h"
+#include "constants/constants.h"
 
 using namespace Constants;
 
-class Elevator : public System
-{
-
+class Elevator : public System {
+private:
+    const int step_pin_;
+    const int dir_pin_;
+    
+    float current_height_ = 0.0; // in cm
+    
+    // Method to convert a distance in centimeters to the corresponding number of steps. Returns number of steps required to move the given distance
+    int translateCmToSteps(float distance);
+    
 public:
-    Elevator();
-    void begin() override;
+    // Constructor with initialization list
+    Elevator(int leftStepPin, int leftDirPin, int rightStepPin, int rightDirPin);
+
+    // Method to initialize the elevator system
+    bool setup();
+
+    // Method to move the elevator up by a specified distance in cm
+    void moveUp(float distance);
+
+    // Method to move the elevator down by a specified distance in cm
+    void moveDown(float distance);
+
+    // Method to move to a specific height in cm
+    void moveToHeight(float height);
+
+    // Method to stop the motors
+    void stop();
 
     void update() override;
     void setState(int state) override;
-    int getCurrentPosition();
 
-private:
-    void Move(int steps);
-    void resetPosition(double position);
-
-    enum class ElevatorState
-    {
+    enum class ElevatorState {
         HOME = 0,
-        LOWER = 2,
-        MID = 3,
-        UPPER = 4,
+        LOW_POSITION = 1,
+        MID_POSITION = 2,
+        HIGH_POSITION = 3,
     };
 
     ElevatorState elevator_state_ = ElevatorState::HOME;
-
-    int current_position_ = 0;
-    int target_position_ = 0;
 };
+
+#endif // ELEVATOR_H
