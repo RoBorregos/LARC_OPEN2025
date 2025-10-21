97f3bb1ebb73a7bc36d4dc66886d1bad18a2faaf
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2025-10-20 19:17:29 -0600

    feat: implement LineSensorChecker for improved line detection logic

--- src/subsystem/LineSensor/LineSensor.hpp
+++ src/subsystem/LineSensor/LineSensor.hpp
@@ -6,30 +6,39 @@
 #include "../systems/system.hpp"
 #include "constants/constants.h"
 #include "constants/pins.h"
+#include "../../lib/sensors/LineSensorChecker.h"
 
 using namespace Constants;
 
-class LineSensor : public System{
-    public:
-        LineSensor();
-        void begin() override;
-        void update() override;
-        void setState(int state) override;
-        std::vector<int> readSensors() const;
-        void printSensors() const;
+class LineSensor : public System
+{
+public:
+    LineSensor();
+    void begin() override;
+    void update() override;
+    void setState(int state) override;
+    std::vector<int> readSensors() const;
+    void printSensors() const;
 
-        bool isFrontLine() const;
-        bool isBackLine() const;
-        bool isLeftLine() const;
-        bool isRightLine() const;
+    bool isFrontLine();
+    bool isBackLine();
+    bool isLeftLine();
+    bool isRightLine();
 
-        bool isFrontLeftLine() const;
-        bool isFrontRightLine() const;
-        bool isBackLeftLine() const;
-        bool isBackRightLine() const;
+    bool isFrontLeftLine() const;
+    bool isFrontRightLine() const;
+    bool isBackLeftLine() const;
+    bool isBackRightLine() const;
 
-    private:
-        int readSensor(int kSensor);
+private:
+    int readSensor(int kSensor);
+
+    LineSensorChecker leftLineChecker{
+        [this]()
+        { return this->isFrontLeftLine(); },
+        [this]()
+        { return this->isBackLeftLine(); },
+        1};
 };
 
 #endif
