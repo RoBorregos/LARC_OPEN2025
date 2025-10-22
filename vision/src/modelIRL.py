75f864d815394e1b97506db79a4518db63807e0f
Lalo <h.tovarm7@gmail.com>
2025-10-22 12:14:21 -0600

    refactor: model loading and camera settings in modelIRL.py

--- vision/src/modelIRL.py
+++ vision/src/modelIRL.py
@@ -3,18 +3,18 @@ from ultralytics import YOLO
 import cv2
 
 # Load model once
-model = YOLO("model/nanoModel.engine")
+model = YOLO("model/nanoModel.pt")
 IGNORE_CLASS = "green bean" 
 class_names = model.names  
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(2)
 if not cap.isOpened():
     print("Can't open camera.")
     exit()
 
 # Set camera properties for better performance
 cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
-cap.set(cv2.CAP_PROP_FPS, 60)
+cap.set(cv2.CAP_PROP_FPS, 30)
 
 print("ESC to exit.")
 
@@ -27,7 +27,7 @@ try:
         # Run inference
         results = model(frame, verbose=False)
         
-        # Display results
+        # Display results if you are using a laptop
         annotated_img = results[0].plot()
         cv2.imshow('Detection', annotated_img)
         
