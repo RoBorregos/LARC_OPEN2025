c7615f027ec46ddf52cf6ac61f0f4b3b16f598f6
Héctor Tovar <h.tovarm07@gmail.com>
2025-10-27 17:24:29 -0600

    refactor: simplify readCommunication and getMatrix methods, remove unused sendData and sendMessage functions

--- src/subsystem/Communication/Communication.cpp
+++ src/subsystem/Communication/Communication.cpp
@@ -25,53 +25,26 @@ string Communication::getCommand() {
 }
 
 string Communication::readCommunication() {
-    String msg;
-
     if (Serial.available()) {
-        msg = Serial.readStringUntil('\n');
+        String msg = Serial.readStringUntil('\n');
+        msg.trim();
         Serial.println(msg);
-        return msg.c_str();
+        return string(msg.c_str()); 
     }
     return "";
 }
 
-
-void Communication::sendMessage(const string &msg){
-    Serial.println(msg.c_str());
-}
-
-void Communication::sendData(const string &payload){
-    string msg = string("DATA:") + payload;
-    sendMessage(msg);
-
-    string reply = readCommunication();
-    if(!reply.empty()){
-        Serial.print("Jetson reply: ");
-        Serial.println(reply.c_str());
-    }
-}
-
 vector<int> Communication::getMatrix() {
     string s = getCommand();
+    if (s.empty()) return {-1, -1};
 
-    if (!s.empty() && s.front() == '[') s.erase(0,1);
-    if (!s.empty() && s.back() == ']') s.pop_back();
-
-    s.erase(std::remove_if(s.begin(), s.end(), ::isspace), s.end());
+    s.erase(remove_if(s.begin(), s.end(), ::isspace), s.end());
 
     size_t comma = s.find(',');
     if (comma == string::npos) return {-1, -1};
 
-    string topStr = s.substr(0, comma);
-    string bottomStr = s.substr(comma + 1);
+    int top = stoi(s.substr(0, comma));
+    int bottom = stoi(s.substr(comma + 1));
 
-    int valTop = -1, valBottom = -1;
-    try {
-        valTop = std::stoi(topStr);
-        valBottom = std::stoi(bottomStr);
-    } catch (...) {
-        return {-1, -1};
-    }
-
-    return {valTop, valBottom};
+    return {top, bottom};
 }
