b7354abdabbdb2bca53e535a15ac77400b4d8e12
Héctor Tovar <h.tovarm07@gmail.com>
2025-10-27 17:18:07 -0600

    feat: added the getMatrix function

--- src/subsystem/Communication/Communication.cpp
+++ src/subsystem/Communication/Communication.cpp
@@ -1,32 +1,28 @@
 #include "Communication.hpp"
 #include <string.h>
+#include <algorithm>
 using namespace std;
 
 Communication::Communication(){
-    Serial.begin(115200);       // USB serial (para debuggear)
-    Serial6.begin(115200);      // UART on pins 25 (RX) and 24 (TX), UART6
+    Serial.begin(115200);       // USB serial
 }
 
 void Communication::begin() {
-    mode_ = Mode::START;
-    startMode();
+    Serial.println("TEENSY READY");
+}
+
+void Communication::update() {
+}
+
+void Communication::setState(int state) {
+    (void)state;
 }
 
 string Communication::getCommand() {
     string msg = readCommunication();
 
-    if(msg.find('[') != string::npos && msg.find(']') != string::npos){
-        // Process matrix data
-        // intake_.doPose(msg);
-    }else if(msg == "R_BENEFIT"){
-        // drive_.goBenefit(R);
-    }else if(msg == "B_BENEFIT"){
-        // drive_.goBenefit(B);
-    }
-
     return msg;
 }
-
 
 string Communication::readCommunication() {
     String msg;
@@ -42,22 +38,9 @@ string Communication::readCommunication() {
     return "";
 }
 
-void Communication::update() {
-}
-
-void Communication::setState(int state) {
-    (void)state;
-}
 
 void Communication::sendMessage(const string &msg){
-    Serial6.print(msg.c_str());
-    if(msg.back() != '\n') Serial6.print('\n');
-    Serial.print("Sent to Jetson: ");
     Serial.println(msg.c_str());
-}
-
-void Communication::startMode(){
-    sendMessage("START");
 }
 
 void Communication::sendData(const string &payload){
@@ -69,4 +52,29 @@ void Communication::sendData(const string &payload){
         Serial.print("Jetson reply: ");
         Serial.println(reply.c_str());
     }
+}
+
+vector<int> Communication::getMatrix(const string &msg) {
+    string s = msg;
+
+    if (!s.empty() && s.front() == '[') s.erase(0,1);
+    if (!s.empty() && s.back() == ']') s.pop_back();
+
+    s.erase(std::remove_if(s.begin(), s.end(), ::isspace), s.end());
+
+    size_t comma = s.find(',');
+    if (comma == string::npos) return {-1, -1};
+
+    string topStr = s.substr(0, comma);
+    string bottomStr = s.substr(comma + 1);
+
+    int valTop = -1, valBottom = -1;
+    try {
+        valTop = std::stoi(topStr);
+        valBottom = std::stoi(bottomStr);
+    } catch (...) {
+        return {-1, -1};
+    }
+
+    return {valTop, valBottom};
 }
